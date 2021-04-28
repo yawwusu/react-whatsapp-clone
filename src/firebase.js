63a3b4f06c2624be85d1d5be182b8f43435a92b1
yawwusu <yawwusugh@gmail.com>
2021-04-28 13:23:35 +0200

    Initial changes: removed logo and set up structure

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC1XpLYvcEp7vVcAG41z_cdIP66-vjNsN4",
+    authDomain: "react-whatsapp-clone-f08e6.firebaseapp.com",
+    projectId: "react-whatsapp-clone-f08e6",
+    storageBucket: "react-whatsapp-clone-f08e6.appspot.com",
+    messagingSenderId: "510396235598",
+    appId: "1:510396235598:web:b933f48a5f0eccb1d8a81f",
+    measurementId: "G-64G7T0VGF6"
+  };
