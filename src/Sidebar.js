768260018029dafb424d9696e60dbd87b643e0fe
yawwusu <yawwusugh@gmail.com>
2021-04-29 11:08:27 +0200

    Sidebar header and search styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import './Sidebar.css'
-import Avatar from '@material-ui/core/Avatar'
+import { Avatar, IconButton } from '@material-ui/core'
 import ChatIcon from '@material-ui/icons/Chat'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import  SearchOutlined  from '@material-ui/icons/SearchOutlined'
 
 function Sidebar() {
     return (
         <div className="Sidebar">
             <div className="Sidebar-header">
                 <Avatar />
-                <DonutLargeIcon />
-                <ChatIcon />
-                <MoreVertIcon />
+                <div className="Sidebar-header-right">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="Sidebar-search">
-
+                <div className="Sidebar-search-container">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat" type="text" />
+                </div>
             </div>
             
             <div className="Sidebar-chats">
